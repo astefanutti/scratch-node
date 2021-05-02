ed73b3ccc12bb4bbaf141ec18655f041318732c8
Antonin Stefanutti <antonin@stefanutti.fr>
2021-05-02 11:31:22 +0200

    Disable LTO for Assembly sources

--- patch.sh
+++ patch.sh
@@ -5,11 +5,15 @@ set -eu
 apply() {
     local arch="$1"
     local version="$2"
-    case "${arch}" in
-        "arm32v6" | "arm32v7")
-            if [[ "$version" =~ ^13\.10\.[0-9]+$ ]]; then
+
+    case "${version}" in
+        13.10.*)
+            if [[ "$arch" = "arm32v6" ]] || [[ "$arch" = "arm32v7" ]]; then
                 patch -p0 < /patches/v8-missing-elf-arm32v6-7.patch
             fi
+            ;;
+        15.*.* | 16.0.0)
+            patch -p0 < /patches/v8-cppgc-shared-no-lto.patch
             ;;
         *)
             ;;
